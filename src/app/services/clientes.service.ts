5f44e1b3333214b5c17481ebea50f423c87b4ca1
Giacca90 <giacca90mobile@gmail.com>
2024-03-24 13:32:05 +0100

    Inicio

---
+++ src/app/services/clientes.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ClienteDinamico } from '../agregar/agregar.component';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ClientesService {
+	public clientes:Array<ClienteDinamico> = [];
+	constructor() { }
+
+	addCliente(cliente:ClienteDinamico) {
+		this.clientes.push(cliente);
+	}
+
+	getClienteForId(id:number) {
+		for(const cliente of this.clientes) {
+			if(cliente.id === id) {
+				return cliente;
+			}
+		}
+		return null;
+	}
+}
+
+
